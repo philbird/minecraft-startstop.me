11abe477837d8754988c49b3621afc1b2573fa9f
SND\startstop_cp <SND\startstop_cp@593edd63-b354-4607-8468-2ae7d70a7900>
2012-01-23 11:31:45 +0000

    Start work on the new startstop webservice implementation integration.

--- WinUploader/WinUploader/Form1.Designer.cs
+++ WinUploader/WinUploader/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.btnSelfLocate = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.lblMessage = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -118,7 +119,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(13, 156);
+            this.linkLabel1.Location = new System.Drawing.Point(10, 176);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(253, 13);
             this.linkLabel1.TabIndex = 8;
@@ -137,11 +138,22 @@
             this.lblMessage.Text = "Upload Complete!";
             this.lblMessage.Visible = false;
             // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(293, 142);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(172, 23);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login to Startstop";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 262);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblMessage);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.btnSelfLocate);
@@ -172,6 +184,7 @@
         private System.Windows.Forms.Button btnSelfLocate;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- WinUploader/WinUploader/Form1.cs
+++ WinUploader/WinUploader/Form1.cs
@@ -7,21 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq; 
+using Newtonsoft.Json.Linq;
 using System.IO;
 using JSonDeserialiser;
-using System.Diagnostics; 
+using System.Diagnostics;
 
 namespace WinUploader
 {
     public partial class Form1 : Form
     {
 
+        #region deprecated calls
         // Setup a connection to the statdrop webservices
         public statdropws.DeveloperAPI oDevAPI = new statdropws.DeveloperAPI();
-        
+
         //set up the return message
-        public statdropws.ReturnMessage oReturnMessage = new statdropws.ReturnMessage(); 
+        public statdropws.ReturnMessage oReturnMessage = new statdropws.ReturnMessage();
+        #endregion
+
+        #region New Service Calls
+
+        public startstop.AccessPoint StartStopAccess = new startstop.AccessPoint();
+        public startstop.MessageResponse MessageResponse = new startstop.MessageResponse();
+        public startstop.ValidatedUserInfo ValidatedUserInfo = new startstop.ValidatedUserInfo();
+
+        #endregion
 
         // Developer APIKEY
         const string APIKey = "1b50f643-fb89-4d5a-8fcf-20ca96deef22";
@@ -50,127 +60,150 @@ namespace WinUploader
             // Instead of using this lets grab the correct path
             string path =
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft\\stats\\";
-            
 
-            string[] files = Directory.GetFiles(path); 
+
+            string[] files = Directory.GetFiles(path);
             // Look for the .DAT files
             foreach (string fileInDir in files)
             {
                 if (fileInDir.ToLower().Contains(".dat"))
                 {
                     // Found the minecraft file
-                    txbFileLocation.Text = fileInDir; 
+                    txbFileLocation.Text = fileInDir;
                 }
             }
         }
 
         private void btnUpload_Click(object sender, EventArgs e)
         {
-           
-            oReturnMessage = oDevAPI.AuthenticateUser(APIKey, txbEmailAddress.Text, txbPassword.Text);
-            if (!oReturnMessage.Success)
+
+            // Validate the user
+            ValidatedUserInfo = StartStopAccess.LoginUser(APIKey, txbEmailAddress.Text, txbPassword.Text);
+            // Check to see if the user is validated
+            if (!ValidatedUserInfo.Validated) // User details invalid
             {
+                if ((txbEmailAddress.Text == "") || (txbPassword.Text == ""))
+                {
+                    MessageBox.Show("You need to provide your email and password to upload");
+                }
+                else
+                {
+                    MessageBox.Show("Login not successful, please login.");
+                }
+            }
+            else
+            {
+                Int64 _UserID = oReturnMessage.ReturnedID;
 
-         
-         
-            
-             if ((txbEmailAddress.Text == "") || (txbPassword.Text == ""))
-             {
-                 MessageBox.Show("You need to provide your email and password to upload"); 
-             }
-             else
-             {
-                 MessageBox.Show("Login not successful, please login.");
-             }
-         }
-         else
-         {
-             Int64 _UserID = oReturnMessage.ReturnedID; 
+                #region Setup the Json file to be parsed... this is dirty, very dirty. ;)
 
-             #region Setup the Json file to be parsed... this is dirty, very dirty. ;)
+                StreamReader streamReader = new StreamReader(txbFileLocation.Text);
+                // We should read this by line so we can add key values into the JSON/ 
+                string _StatsFile = "";
+                bool _hitStatsChange = false;
+                while (!streamReader.EndOfStream)
+                {
+                    string _holdLine = streamReader.ReadLine();
 
-             StreamReader streamReader = new StreamReader(txbFileLocation.Text);
-             // We should read this by line so we can add key values into the JSON/ 
-             string _StatsFile = "";
-             bool _hitStatsChange = false;
-             while (!streamReader.EndOfStream)
-             {
-                 string _holdLine = streamReader.ReadLine();
+                    // Capture the end of the file. 
+                    if ((_hitStatsChange) && (_holdLine.Contains("],")))
+                    {
 
-                 // Capture the end of the file. 
-                 if ((_hitStatsChange) && (_holdLine.Contains("],")))
-                 {
-
-                     _hitStatsChange = false;
-                 }
+                        _hitStatsChange = false;
+                    }
 
 
-                 if (_hitStatsChange)
-                 {
-                     // Add the Value
-                     _holdLine = _holdLine.Replace("\":", "\", \"Val\":");
-                     // Add the Key
-                     _holdLine = _holdLine.Replace("{\"", "{\"Key\":\"");
+                    if (_hitStatsChange)
+                    {
+                        // Add the Value
+                        _holdLine = _holdLine.Replace("\":", "\", \"Val\":");
+                        // Add the Key
+                        _holdLine = _holdLine.Replace("{\"", "{\"Key\":\"");
 
-                 }
-                 // This has to go below the above so we don't capure the initia line. 
-                 if (_holdLine.ToLower().Contains("stats-change"))
-                 {
-                     _hitStatsChange = true;
-                     // This is a dirty aweful hack this whole lot. 
-                     _holdLine = _holdLine.Replace("stats-change", "statschange");
-                 }
-                 _StatsFile += _holdLine + System.Environment.NewLine;
+                    }
+                    // This has to go below the above so we don't capure the initia line. 
+                    if (_holdLine.ToLower().Contains("stats-change"))
+                    {
+                        _hitStatsChange = true;
+                        // This is a dirty aweful hack this whole lot. 
+                        _holdLine = _holdLine.Replace("stats-change", "statschange");
+                    }
+                    _StatsFile += _holdLine + System.Environment.NewLine;
 
-             }
+                }
 
-             streamReader.Close();
-             #endregion
+                streamReader.Close();
+                #endregion
 
-             var jsonObject = JsonConvert.DeserializeObject<JsonObject>(_StatsFile);
+                var jsonObject = JsonConvert.DeserializeObject<JsonObject>(_StatsFile);
 
-             int _statsCount = 0;
-             while (_statsCount < jsonObject.statschange.Count)
-             {
-                 string _Key = jsonObject.statschange[_statsCount].Key;
-                 int _Val = jsonObject.statschange[_statsCount].Val;
+                int _statsCount = 0;
+                while (_statsCount < jsonObject.statschange.Count)
+                {
+                    string _Key = jsonObject.statschange[_statsCount].Key;
+                    int _Val = jsonObject.statschange[_statsCount].Val;
 
-                 #region UploadTheStat
+                    #region UploadTheStat
 
-                 // Load the dictionary
-                 MineCraftHelper.Stats oStats = new MineCraftHelper.Stats();
-
-
-                 if (_UserID != -1)
-                 {
-                     if (oStats.ReturnStartStopID(_Key) != -1)
-                     {
-                         oDevAPI.ExactStatUpdateForUser(APIKey, _UserID, oStats.ReturnStartStopID(_Key), _Val, 0, "");
-                     }
-
-                 }
+                    // Load the dictionary
+                    MineCraftHelper.Stats oStats = new MineCraftHelper.Stats();
 
 
-                 #endregion
+                    if (_UserID != -1)
+                    {
+                        if (oStats.ReturnStartStopID(_Key) != -1)
+                        {
+                            oDevAPI.ExactStatUpdateForUser(APIKey, _UserID, oStats.ReturnStartStopID(_Key), _Val, 0, "");
+                        }
 
-                 _statsCount++;
-             }
+                    }
+
+
+                    #endregion
+
+                    _statsCount++;
+                }
                 // Update the last updated
-             oDevAPI.ExactStatUpdateForUser(APIKey, _UserID, 34, 0 , 0, DateTime.Now.ToString());
-             lblMessage.Visible = true; 
-         } // End Not Logged in IF
-       
+                oDevAPI.ExactStatUpdateForUser(APIKey, _UserID, 34, 0, 0, DateTime.Now.ToString());
+                lblMessage.Visible = true;
+            } // End Not Logged in IF
+
         }
 
         private void btnSelfLocate_Click(object sender, EventArgs e)
         {
-             openFileDialog1.ShowDialog();
-            txbFileLocation.Text = openFileDialog1.FileName; 
+            openFileDialog1.ShowDialog();
+            txbFileLocation.Text = openFileDialog1.FileName;
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Process.Start("http://www.statstop.me/Register.aspx");
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Validate the user
+            ValidatedUserInfo = StartStopAccess.LoginUser(APIKey, txbEmailAddress.Text, txbPassword.Text);
+            // Check to see if the user is validated
+            if (!ValidatedUserInfo.Validated) // User details invalid
+            {
+                if ((txbEmailAddress.Text == "") || (txbPassword.Text == ""))
+                {
+                    MessageBox.Show("You need to provide your email and password to upload");
+                }
+                else
+                {
+                    MessageBox.Show("Login not successful, please login.");
+                }
+            }
+            else
+            {
+                // Once logged in we can disable all the stuff
+                txbPassword.Enabled = false;
+                txbEmailAddress.Enabled = false;
+                btnLogin.Enabled = false; 
+            }
         }
     }
 }
