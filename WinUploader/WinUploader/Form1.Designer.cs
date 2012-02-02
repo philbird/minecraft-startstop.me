23e83350e6f112f991122a6ff872e2681cf1b58e
SND\startstop_cp <SND\startstop_cp@593edd63-b354-4607-8468-2ae7d70a7900>
2012-02-02 09:57:39 +0000

    Tidied up the form a little bit, just need to finalise the stats linking.

--- WinUploader/WinUploader/Form1.Designer.cs
+++ WinUploader/WinUploader/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnLocate = new System.Windows.Forms.Button();
             this.txbFileLocation = new System.Windows.Forms.TextBox();
@@ -40,6 +41,7 @@
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.lblMessage = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -49,24 +51,31 @@
             // 
             // btnLocate
             // 
-            this.btnLocate.Location = new System.Drawing.Point(290, 12);
+            this.btnLocate.BackColor = System.Drawing.Color.Transparent;
+            this.btnLocate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLocate.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocate.ForeColor = System.Drawing.Color.White;
+            this.btnLocate.Location = new System.Drawing.Point(13, 129);
             this.btnLocate.Name = "btnLocate";
-            this.btnLocate.Size = new System.Drawing.Size(175, 23);
+            this.btnLocate.Size = new System.Drawing.Size(117, 23);
             this.btnLocate.TabIndex = 0;
             this.btnLocate.Text = "Find Minecraft Stats";
-            this.btnLocate.UseVisualStyleBackColor = true;
+            this.btnLocate.UseVisualStyleBackColor = false;
             this.btnLocate.Click += new System.EventHandler(this.button1_Click);
             // 
             // txbFileLocation
             // 
-            this.txbFileLocation.Location = new System.Drawing.Point(12, 12);
+            this.txbFileLocation.BackColor = System.Drawing.Color.Gainsboro;
+            this.txbFileLocation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbFileLocation.Location = new System.Drawing.Point(12, 103);
             this.txbFileLocation.Name = "txbFileLocation";
-            this.txbFileLocation.Size = new System.Drawing.Size(272, 20);
+            this.txbFileLocation.Size = new System.Drawing.Size(230, 20);
             this.txbFileLocation.TabIndex = 1;
             // 
             // btnUpload
             // 
-            this.btnUpload.Location = new System.Drawing.Point(290, 227);
+            this.btnUpload.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(67, 311);
             this.btnUpload.Name = "btnUpload";
             this.btnUpload.Size = new System.Drawing.Size(175, 23);
             this.btnUpload.TabIndex = 2;
@@ -76,62 +85,75 @@
             // 
             // txbEmailAddress
             // 
-            this.txbEmailAddress.Location = new System.Drawing.Point(12, 77);
+            this.txbEmailAddress.Location = new System.Drawing.Point(12, 171);
             this.txbEmailAddress.Name = "txbEmailAddress";
-            this.txbEmailAddress.Size = new System.Drawing.Size(272, 20);
+            this.txbEmailAddress.Size = new System.Drawing.Size(230, 20);
             this.txbEmailAddress.TabIndex = 3;
             // 
             // txbPassword
             // 
-            this.txbPassword.Location = new System.Drawing.Point(13, 113);
+            this.txbPassword.Location = new System.Drawing.Point(13, 210);
             this.txbPassword.Name = "txbPassword";
-            this.txbPassword.Size = new System.Drawing.Size(271, 20);
+            this.txbPassword.Size = new System.Drawing.Size(229, 20);
             this.txbPassword.TabIndex = 4;
+            this.txbPassword.UseSystemPasswordChar = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(290, 80);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 155);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.Size = new System.Drawing.Size(157, 12);
             this.label1.TabIndex = 5;
-            this.label1.Text = "StartStop.me Registered Email ";
+            this.label1.Text = "Email address or username";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(290, 116);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 194);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.Size = new System.Drawing.Size(59, 12);
             this.label2.TabIndex = 6;
-            this.label2.Text = "StartStop.me Password";
+            this.label2.Text = "Password";
             // 
             // btnSelfLocate
             // 
-            this.btnSelfLocate.Location = new System.Drawing.Point(290, 42);
+            this.btnSelfLocate.BackColor = System.Drawing.Color.Transparent;
+            this.btnSelfLocate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSelfLocate.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelfLocate.ForeColor = System.Drawing.Color.White;
+            this.btnSelfLocate.Location = new System.Drawing.Point(135, 129);
             this.btnSelfLocate.Name = "btnSelfLocate";
-            this.btnSelfLocate.Size = new System.Drawing.Size(175, 23);
+            this.btnSelfLocate.Size = new System.Drawing.Size(107, 23);
             this.btnSelfLocate.TabIndex = 7;
             this.btnSelfLocate.Text = "Locate Yourself";
-            this.btnSelfLocate.UseVisualStyleBackColor = true;
+            this.btnSelfLocate.UseVisualStyleBackColor = false;
             this.btnSelfLocate.Click += new System.EventHandler(this.btnSelfLocate_Click);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(10, 176);
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(49, 9);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(253, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(151, 13);
             this.linkLabel1.TabIndex = 8;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "If you do not have a startstop.me account click here";
+            this.linkLabel1.Text = "Click to register for an account";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
             this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessage.Location = new System.Drawing.Point(16, 189);
+            this.lblMessage.Location = new System.Drawing.Point(35, 283);
             this.lblMessage.Name = "lblMessage";
             this.lblMessage.Size = new System.Drawing.Size(199, 25);
             this.lblMessage.TabIndex = 9;
@@ -140,19 +162,35 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(293, 142);
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(70, 236);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(172, 23);
             this.btnLogin.TabIndex = 10;
             this.btnLogin.Text = "Login to Startstop";
-            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(159, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Version : 2nd Feb 2012";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 262);
+            this.BackgroundImage = global::WinUploader.Properties.Resources.ssbackground;
+            this.ClientSize = new System.Drawing.Size(262, 352);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblMessage);
             this.Controls.Add(this.linkLabel1);
@@ -164,8 +202,10 @@
             this.Controls.Add(this.btnUpload);
             this.Controls.Add(this.txbFileLocation);
             this.Controls.Add(this.btnLocate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Minecraft Stats uploader for Startstop.me";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,6 +225,7 @@
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label lblMessage;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
     }
 }
 
